8100c8f12cbbf6d62c6f9e82a4a5d0526b0e0df5
Madhur Arora <anunforgettableusername@gmail.com>
2023-03-15 17:56:00 -0700

    Camera Update
    - Camera Follows the Player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,7 +5,14 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f; 
     [SerializeField] private float zoomSpeed = 5f; 
     [SerializeField] private float minZoom = 1f; 
-    [SerializeField] private float maxZoom = 5f; 
+    [SerializeField] private float maxZoom = 5f;
+
+    [Space, Header("Game State")]
+    [SerializeField]
+    private GameState _gameState;
+
+    [SerializeField]
+    private Transform _playerTransform;
 
     private Vector3 dragOrigin;
 
@@ -28,5 +35,21 @@ public class CameraController : MonoBehaviour
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         float zoomAmount = Mathf.Clamp(Camera.main.orthographicSize - scroll * zoomSpeed, minZoom, maxZoom);
         Camera.main.orthographicSize = zoomAmount;
+
+        //MoveCameraToCenter();
+        FollowPlayer();
+    }
+
+    private void MoveCameraToCenter()
+    {
+        if (_gameState.Value == States.DIALOGUE)
+        {
+            transform.position = new Vector3(_playerTransform.position.x + 3f, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = new Vector3(_playerTransform.position.x, transform.position.y, transform.position.z);
     }
 }
