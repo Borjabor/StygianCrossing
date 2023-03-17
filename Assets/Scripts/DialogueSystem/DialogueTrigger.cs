42ae65c1d7b79a4ec080f3569990f843125b961d
André Borja <andre.borja.miranda@gmail.com>
2023-03-16 19:02:00 -0700

    Articy up and running

--- Assets/Scripts/DialogueSystem/DialogueTrigger.cs
+++ Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -1,49 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Articy.Unity;
 using UnityEngine;
 using UnityEngine.UI;
 
-//[RequireComponent(typeof(Outline))]
+[RequireComponent(typeof(ArticyReference))]
 public class DialogueTrigger : MonoBehaviour, IInteractable
 {
-    private Outline _outline;
-
-    [SerializeField] protected TextAsset _dialogue;
+    protected ArticyObject _dialogue;
+    
     [SerializeField] protected GameState _gameState;
-    [Tooltip("The sprite you want to use as the portrait for the character")]
-    [SerializeField] protected Sprite _portraitImage;
-    [Tooltip("Drag the Portrait object from within Dialogue Panel > Image > Portrait")]
-    [SerializeField] protected Image _portrait;
 
     [SerializeField] private GameObject _prompt;
-    
-    
 
     protected void Awake()
     {
-        //_outline = GetComponent<Outline>();
-        //_outline.OutlineWidth = 0f;
+        _prompt.SetActive(false);
+        _dialogue = GetComponent<ArticyReference>().reference.GetObject();
     }
 
     public virtual void Interact()
     {
         if(_gameState.Value is States.DIALOGUE or States.PAUSED) return;
         DialogueManager.GetInstance().ClearPlayer();
-        DialogueManager.GetInstance().EnterDialogueMode(_dialogue);
-        _portrait.sprite = _portraitImage;
+        DialogueManager.GetInstance().EnterDialogue(_dialogue);
     }
 
     public void ShowPrompt()
     {
-        //_outline.OutlineColor = Color.yellow;
-        //_outline.OutlineWidth = 10f;
         _prompt.SetActive(true);
     }
 
     public void HidePrompt()
     {
-        //_outline.OutlineWidth = 0f;
         _prompt.SetActive(false);
     }
 }
