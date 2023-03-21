ff76acaa440d8bc018710c9a4258c49f7ba0cc6c
André Borja <andre.borja.miranda@gmail.com>
2023-03-21 13:02:05 -0700

    Change to var listener

--- Assets/Scripts/DialogueSystem/GlobalVariableListener.cs
+++ Assets/Scripts/DialogueSystem/GlobalVariableListener.cs
@@ -5,17 +5,17 @@ using Articy.UnityImporterTutorial.GlobalVariables;
 using UnityEditor;
 using UnityEngine;
 
-public class GlobalVariableListener : MonoBehaviour
+public abstract class GlobalVariableListener : MonoBehaviour
 {
     protected virtual void Start()
     {
         // we get the default variable storage from our database and add a listener to the notification manager
         // to call our supplied method every time any Variable inside the namespace "GameState" is changed.
-        ArticyDatabase.DefaultGlobalVariables.Notifications.AddListener("GameState.*", MyGameStateVariablesChanged);
+        ArticyDatabase.DefaultGlobalVariables.Notifications.AddListener("GameState.*", OnGlobalVariablesChanged);
     }
     
     // here we handle every variable change inside the "GameState" namespace
-    protected virtual void MyGameStateVariablesChanged(string aVariableName, object aValue)
+    protected virtual void OnGlobalVariablesChanged(string aVariableName, object aValue)
     {
         /*if (aVariableName == "VariableSet.VariableName" && (type)aValue == desiredValue)
         {
