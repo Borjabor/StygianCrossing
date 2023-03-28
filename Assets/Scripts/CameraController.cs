248bc84b690bedee81a6a6ef93f4b4ce0ebe5413
Madhur Arora <anunforgettableusername@gmail.com>
2023-03-27 19:01:56 -0700

    Camera Test
    - Updated Camera
    - Doesn't Work rn

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f; 
-    [SerializeField] private float zoomSpeed = 5f; 
-    [SerializeField] private float minZoom = 1f; 
-    [SerializeField] private float maxZoom = 5f;
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private float zoomSpeed = 5f;
+
+    [SerializeField]
+    private float minZoom = 1f;
+
+    [SerializeField]
+    private float maxZoom = 5f;
 
     [Space, Header("Game State")]
     [SerializeField]
@@ -18,23 +27,27 @@ public class CameraController : MonoBehaviour
     private Vector3 dragOrigin;
 
     private bool _inDialogue = false;
+    private bool _newPosition = false;
+
+    int value = 0;
+
     private float _moveSpeed = 1f;
 
     private Vector3 _targetPosition;
 
     private void Start()
     {
-        _targetPosition = new Vector3(transform.position.x + 3f, transform.position.y, transform.position.z);
     }
 
     void Update()
     {
         //PanAndZoom();
+
         MoveCameraToCenter();
-        
+
         if (!_inDialogue)
         {
-            FollowPlayer();  
+            FollowPlayer();
         }
     }
 
@@ -59,17 +72,17 @@ public class CameraController : MonoBehaviour
         Camera.main.orthographicSize = zoomAmount;
     }
 
+
     private void MoveCameraToCenter()
     {
         float step = _moveSpeed * Time.deltaTime;
         
-        
         if (_gameState.Value == States.DIALOGUE)
         {
-            _inDialogue = true;
+            _targetPosition = new Vector3(transform.position.x + 5f, transform.position.y, transform.position.z);
             transform.position = Vector3.MoveTowards(transform.position, _targetPosition, step);
         }
-        
+
         if (_gameState.Value == States.NORMAL)
         {
             _inDialogue = false;
