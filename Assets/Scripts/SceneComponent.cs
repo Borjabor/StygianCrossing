2af0a33d77aba0cf8f58500c1db622ca2442a23b
Madhur Arora <anunforgettableusername@gmail.com>
2023-03-29 14:06:33 -0700

    Scene Component Update
    - Plays Audio when the scene is switched
    - Scene Locked Enabled

--- Assets/Scripts/SceneComponent.cs
+++ Assets/Scripts/SceneComponent.cs
@@ -4,12 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-
 public class SceneComponent : MonoBehaviour
 {
-
     public bool _isActive;
-    
+
     [SerializeField]
     private bool _isLocked = false;
 
@@ -17,6 +15,8 @@ public class SceneComponent : MonoBehaviour
     private Light[] _SceneLights;
 
     private AudioSource _as;
+
+    public bool _playSound = false;
 
     private void Start()
     {
@@ -36,7 +36,7 @@ public class SceneComponent : MonoBehaviour
             else
             {
                 InactiveScene();
-            }   
+            }
         }
 
         else
@@ -49,19 +49,34 @@ public class SceneComponent : MonoBehaviour
     {
         Transform overlayTransform = transform.GetChild(0);
         overlayTransform.GetComponent<Renderer>().enabled = false;
-        _as.Play();
+
+        foreach (var light in _SceneLights)
+        {
+            light.enabled = true;
+        }
+
+        if (_playSound)
+        {
+            _as.Play();
+            _playSound = false;
+        }
         //print("This scene is active now");
     }
-    
+
     private void InactiveScene()
     {
         Transform overlayTransform = transform.GetChild(0);
         overlayTransform.GetComponent<Renderer>().enabled = true;
+
+        foreach (var light in _SceneLights)
+        {
+            light.enabled = false;
+        }
+
         _as.Stop();
     }
 
     private void LockedScene()
     {
-        
     }
 }
