67908dd616267645317c0d0e5a1d4ba1bf232c7d
Madhur Arora <anunforgettableusername@gmail.com>
2023-04-03 21:08:09 -0700

    Variable Setup
    - Set up variable objects
    - New Scripts created

---
+++ Assets/Scripts/Interactable Objects/NewsPaper.cs
@@ -0,0 +1,9 @@
+
+public class NewsPaper : InspectableObject
+{
+
+    protected override void ObjectInteraction(string arg1, object arg2)
+    {
+        base.ObjectInteraction("GlobalVariables.newsPaper", true);
+    }
+}
