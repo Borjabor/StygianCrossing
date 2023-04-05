c83303a54ff1335a1837b18a9d407d86c24c43c6
Madhur Arora <anunforgettableusername@gmail.com>
2023-04-05 00:45:43 -0700

    Walter Moves!
    - Walter Moves when variable is triggered

---
+++ Assets/Walter.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walter : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 _newPos;
+    
+    [SerializeField]
+    private GlobalVariableListener _listener;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnEnable()
+    {
+        _listener.GlobalVariableChanged += WalterInteraction;
+    }
+
+    private void OnDisable()
+    {
+        _listener.GlobalVariableChanged -= WalterInteraction;
+    }
+
+    protected virtual void WalterInteraction(string arg1, object arg2)
+    {
+        if (arg1 == $"GlobalVariables.WaltConvo1" && (bool)arg2)
+        {
+            transform.position = _newPos;
+            print("Moving Walter");
+        }
+    }
+}
